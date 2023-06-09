230878f6b4a32cd49785e5a18b7e2efd1100332e
Meryprog <mereprecieux@gmail.com>
2023-06-09 16:17:56 +0200

    Structure de base

---
+++ README.md
@@ -0,0 +1,1 @@
+# merytest first push
---
+++ gestime/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestimeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gestime'
